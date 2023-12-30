c0238ff699dc18103679547415116a720adf11d6
Raphael Scherpinski <rsbrandao@inf.ufrgs.br>
2023-12-30 02:54:07 -0300

    [PAS] Initial Commit

---
+++ src/main/java/br/com/rsbrandao/PetAdoptionSystem/PetAdoptionSystemApplication.java
@@ -0,0 +1,13 @@
+package br.com.rsbrandao.PetAdoptionSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetAdoptionSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetAdoptionSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/rsbrandao/PetAdoptionSystem/PetAdoptionSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rsbrandao.PetAdoptionSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetAdoptionSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
